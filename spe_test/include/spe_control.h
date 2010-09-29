7d9376ac09ee7925d6b480d672d2714e39e19310
Eduardo Marques <eduardo@localhost.localdomain>
2010-09-29 20:51:17 -0300

    adicionando estrutra de projeto para os testes de leitura das spes

---
+++ spe_test/include/spe_control.h
@@ -0,0 +1,4 @@
+#ifndef __SPE_CONTROL_H
+#define __SPE_CONTROL_H
+	
+#endif
---
+++ spe_test/src/spe_control.c
@@ -0,0 +1,8 @@
+#include "spe_control.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	printf("Teste\r\n");
+	return EXIT_SUCCESS;	
+}
